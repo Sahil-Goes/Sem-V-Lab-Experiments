c0f1d0516811762a4e7e481e52c17a6fd0005de9
SahilGoes <sahilgoes87@gmail.com>
2025-06-29 14:39:40 +0530

    first commit

---
+++ bfs_traversal.py
@@ -0,0 +1,25 @@
+#BFS Traversal
+graph = {
+    'A': ['B','C'],
+    'B': ['D'],
+    'C': ['E'],
+    'D': ['F'],
+    'E': ['F'],
+    'F': []
+}
+
+
+def bfs(graph):
+        close =[]
+        open = [start]
+        while open:
+            node = open.pop(0)
+            if node not in close:
+                close.append(node)
+                neighbours = graph[node]
+                for i in neighbours:
+                    open.append(i)
+        return close
+
+start = input("Enter start node: ")
+print("BFS Traversal: ", bfs(graph))
